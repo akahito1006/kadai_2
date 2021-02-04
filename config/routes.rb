28a16c66c6223232b7bbe129a40b207af5891a09
akahito1006 <nekodora5k@gmail.com>
2021-02-04 05:05:48 +0000

    [Update] top

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'tops#top'
+  get 'top' => 'tops#top'
 end
