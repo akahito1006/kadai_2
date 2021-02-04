5224ee40e5ad7bd284e2b096699a07f6a49844c8
akahito1006 <nekodora5k@gmail.com>
2021-02-04 05:20:44 +0000

    [Update] 3-8 1_top_rooted 2_book_linked each routes different controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tops#top'
   get 'top' => 'tops#top'
+  
+  get 'books' => 'books#books'
 end
