2f51cd8c727b914a725a64582fd38ec4d49c65d8
akahito1006 <nekodora5k@gmail.com>
2021-02-06 09:26:30 +0000

    [Update] 3-8 HTML,CSS..., errorchecking

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ class BooksController < ApplicationController
     # そうしないと空のモデルの代入が行われず、createｱｸｼｮﾝによってparamsで:idにbookが代入されてしまい
     # ActionController::ParameterMissing　が出る
   end
-  
+
   # def new
   #   @book = Book.new
   # end
@@ -19,11 +19,15 @@ class BooksController < ApplicationController
   
   def create
     # データ新規登録するためのインスタンス作成
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
     ## データをデータベースに保存するためのsaveメソッド実行
-    book.save
-    # # books画面へリダイレクトする
-    redirect_to booklist_path(book.id)
+    if @book.save
+    # # books画面へリダイレクトする、サクセスメッセージを表示させる
+      redirect_to booklist_path(@book.id), notice: 'Book was successfully created.'
+    else
+      @books = Book.all
+      render :books
+    end
   end
   
   def show
@@ -37,13 +41,13 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to booklist_path(book.id)
+    redirect_to booklist_path(book.id), notice: 'Book was successfully updated.'
   end
   
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path
+    redirect_to books_path, notice: 'Book was successfully destroyed.'
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'books#top'
+  # 嘘でもルートがきちんとした文法で書かれていないとbashもちゃんと動かなかった　controllerを削除することもできない
   get 'top' => 'books#top'
   
   get 'books' => 'books#books'
-  
+
   post 'book' => 'books#create'
-  
+
   # url for form_with on books.html
   # get 'booklists' => 'books#books'
-  
-  get '/:id' => 'books#show', as: 'booklist'
+
+  get '/books/:id' => 'books#show', as: 'booklist'
   # as: booklist"S" is already used on form_with and rails will be aborted,
   # or just booklist will now work fine
-  get '/:id/edit' => 'books#edit', as: 'edit_booklist'
+  get '/books/:id/edit' => 'books#edit', as: 'edit_booklist'
 
   patch '/:id' => 'books#update', as: 'update_booklist'
-  
+
   delete '/:id' => 'books#destroy', as: 'destroy_booklist'
 end
